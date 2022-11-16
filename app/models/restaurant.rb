b54d3e4309af53b384b224fd4f01f8534e61811d
lola martinhao <lola.martinhao@gmail.com>
2022-11-16 16:51:55 -0300

    adding controllers features and routes pages

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,9 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: {
+    in: %w[chinese italian japanese french belgian],
+    message: 'Sorry, this is not a valid category'
+  }
 end
